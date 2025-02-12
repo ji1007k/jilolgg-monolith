44d6cef2ceede80979dbae9f0274a13ae7bbae5c
jungin <wjddls141093@duksung.ac.kr>
2025-02-12 19:40:23 +0900

    Spring Security + JWT 로그인

---
+++ src/main/resources/static/js/auth/jwt/JwtUtil.js
@@ -0,0 +1,72 @@
+/**
+ * JWT 토큰 관리
+ */
+let _expirationTime = null;
+
+// 토큰 발급 (세션쿠키에 저장)
+export async function getTokenWithSessionCookie(data) {
+    const utf8Encoded = String.fromCharCode(...new TextEncoder().encode(data));
+
+    const response = await fetch("/token/generate/sc", {
+        method: "POST",
+        headers: {
+            "Authorization": `Basic ${btoa(utf8Encoded)}`,
+            "Content-Type": "application/json" // 필요에 따라 설정
+        }
+    });
+
+    const result = await response.json();
+
+    // 토큰 만료 시간 초기화
+    _expirationTime = new Date(result.expirationTime);
+    console.log('Token Expiration Time:', _expirationTime);
+
+    return result;
+}
+
+// 액세스 토큰 갱신 요청
+export async function refreshToken() {
+    try {
+        // 리프레시 토큰으로 새로운 액세스 토큰을 갱신
+        const response = await fetch('/token/refresh', {
+            method: 'POST',
+            credentials: 'include', // 쿠키 포함
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to refresh token');
+        }
+
+        // 새로 발급한 토큰 만료 시간 출력
+        const result = await response.json();
+        console.log('Token Expiration Time:', result.expirationTime);
+        _expirationTime = new Date(result.expirationTime); // 예시로, 실제 토큰 생성 시 받아온 만료 시간 사용
+
+        displayExpirationTime();  // 새로 갱신된 만료 시간 표시
+    } catch (error) {
+        console.error('Error refreshing token:', error);
+    }
+}
+
+// 액세스 토큰이 발급될 때 만료 시간을 받아서 화면에 표시하도록 설정
+export function displayExpirationTime(expirationTimeStr) {
+    if (expirationTimeStr) {
+        _expirationTime = new Date(expirationTimeStr);
+    }
+
+    // 토큰 생성 후 받아온 만료 시간을 이용하여 남은 시간을 표시
+    const currentTime = new Date();
+    const timeLeft = _expirationTime - currentTime;
+
+    const timeArea = document.querySelector('#tokenExpirationTime');
+    if (timeLeft <= 0) {
+        timeArea.textContent = "Token Expired";
+    } else {
+        const minutesLeft = Math.floor(timeLeft / 60000);
+        const secondsLeft = Math.floor((timeLeft % 60000) / 1000);
+        timeArea.textContent = `Time Left: ${minutesLeft}m ${secondsLeft}s`;
+    }
+
+    // 1초마다 남은 시간 갱신
+    setInterval(displayExpirationTime, 1000);
+}
---
+++ src/test/java/com/test/basic/HomeControllerTests.java
@@ -0,0 +1,84 @@
+package com.test.basic;
+//package example.web;
+
+/*
+ * Copyright 2002-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.test.basic.config.SecurityConfig;
+import com.test.basic.auth.jwt.TokenController;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Tests for {@link HomeController}
+ *
+ * @author Josh Cummings
+ */
+@WebMvcTest({ HomeController.class, TokenController.class })
+@Import(SecurityConfig.class)
+public class HomeControllerTests {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void rootWhenAuthenticatedThenSaysHelloUser() throws Exception {
+        // @formatter:off
+        MvcResult result = this.mvc.perform(post("/token")
+                        .with(httpBasic("user", "password")))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String token = result.getResponse().getContentAsString();
+
+        /*this.mvc.perform(get("/")
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(content().string("Hello, user!"));*/
+
+        this.mvc.perform(get("/mypage", 1)
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(content().string("Hello, user!"));
+        // @formatter:on
+    }
+
+    @Test
+    void rootWhenUnauthenticatedThen401() throws Exception {
+        // @formatter:off
+        this.mvc.perform(get("/"))
+                .andExpect(status().isUnauthorized());
+        // @formatter:on
+    }
+
+    @Test
+    void tokenWhenBadCredentialsThen401() throws Exception {
+        // @formatter:off
+        this.mvc.perform(post("/token"))
+                .andExpect(status().isUnauthorized());
+        // @formatter:on
+    }
+
+}

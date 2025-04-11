ce2d6f80d5a13b095ed829dd96a359b80098689a
jungin <wjddls141093@duksung.ac.kr>
2025-04-11 18:10:12 +0900

    1. feat(팀 즐겨찾기): 팀 즐겨찾기 API 구현 - 즐겨찾는 팀 목록 조회 - 팀 즐겨찾기 & 삭제

---
+++ src/main/java/com/test/basic/lol/favorites/FavoriteTeamRequest.java
@@ -0,0 +1,12 @@
+package com.test.basic.lol.favorites;
+
+
+import lombok.Data;
+
+// 즐겨찾기 등록 요청
+@Data
+public class FavoriteTeamRequest {
+    private String teamCode;
+    private String teamName;
+}
+

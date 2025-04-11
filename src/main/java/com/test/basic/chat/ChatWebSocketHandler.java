00f51630e40e6f8f2f57cdef016e9f158017540f
jungin <wjddls141093@duksung.ac.kr>
2025-04-11 15:25:51 +0900

    1. refactor(chat): JWT subject 값을 userId로 변경함에 따라, 기존 사용자 식별 변수들도 userId 기준으로 정리

--- src/main/java/com/test/basic/chat/ChatWebSocketHandler.java
+++ src/main/java/com/test/basic/chat/ChatWebSocketHandler.java
@@ -30,8 +30,8 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         // WebSocket 연결이 성립되면 세션에서 인증 정보 가져오기
-        String username = getUserNameFromSession(session);
-        sessions.put(username, session);  // 사용자 세션을 맵에 저장
+        String userId = getUserIdFromSession(session);
+        sessions.put(userId, session);  // 사용자 세션을 맵에 저장
         logger.info("✅ WebSocket 연결됨: {}", session.getId());
 
         // 기존 채팅 불러오기
@@ -44,24 +44,29 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
         // 3️⃣ 새로 입장한 사용자에게만 기존 메시지 전송
         if (messages != null && !messages.isEmpty()) {
             for (String message : messages) {
+                // 메시지 전송 (서버 -> 클라)
                 session.sendMessage(new TextMessage(message)); // 🔥 해당 세션에만 메시지 전송!
             }
         }
     }
 
     @Override
-    @Description("메시지를 받아서 모든 클라이언트에 브로드캐스트")
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
-        // WebSocket 연결이 성립되면 세션에서 인증 정보 가져오기
+    @Description("클라이언트가 보낸 메시지를 받아서 다른 클라이언트에 브로드캐스트")
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) {
+        String userId = getUserIdFromSession(session);
         String username = getUserNameFromSession(session);
         String messageContent = message.getPayload();
-        logger.info("📩 메시지 수신: [{}] {}", username, messageContent);
+        logger.info("📩 메시지 수신: [{}] {}", userId, messageContent);
 
         // 메시지를 Redis로 전송
-        chatService.sendMessage("1", username, new TextMessage(messageContent));
+        chatService.sendMessage("1", userId, username, new TextMessage(messageContent));
     }
 
     // WebSocketSession에 저장된 사용자 인증 정보 반환 (JWT 등)
+    private String getUserIdFromSession(WebSocketSession session) {
+        return (String) session.getAttributes().get("userId");
+    }
+
     private String getUserNameFromSession(WebSocketSession session) {
         return (String) session.getAttributes().get("username");
     }
@@ -75,9 +80,8 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        // WebSocket 연결이 성립되면 세션에서 인증 정보 가져오기
-        String username = getUserNameFromSession(session);
-        sessions.remove(username);  // 세션 종료 시 제거
-        logger.info("❌ WebSocket 연결 종료: [{}] {}", username, session.getId());
+        String userId = getUserIdFromSession(session);
+        sessions.remove(userId);  // 세션 종료 시 제거
+        logger.info("❌ WebSocket 연결 종료: [{}] {}", userId, session.getId());
     }
 }

f9d1b5b79b8a92f19be5aeb39b94b0f75585f5ac
jungin <wjddls141093@duksung.ac.kr>
2025-04-25 15:10:58 +0900

    1. fix(db): 사용자 생성 쿼리 오류 수정 - plsql 사용제한 있을 수 있다고 함

--- src/main/resources/db/postgres/init.sql
+++ src/main/resources/db/postgres/init.sql
@@ -1,17 +1,10 @@
--- 사용자 생성 (이미 있으면 건너뛰기)
-DO $$
-BEGIN
-    -- 사용자 존재 여부 확인
-    IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_roles WHERE rolname = 'jikim') THEN
-        -- 사용자 생성
-        CREATE USER jikim WITH PASSWORD 'jikim';
-    END IF;
-END $$;
+-- 사용자 생성 (이미 있으면 오류 발생하지 않음)
+CREATE USER jikim WITH PASSWORD 'jikim';
 
 -- 데이터베이스 권한 부여
 GRANT ALL PRIVILEGES ON DATABASE basic TO jikim;
 
--- 스키마 내 권한 부여 (기존 권한 부여 여부 확인 불필요)
+-- 스키마 내 권한 부여
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO jikim;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO jikim;
 GRANT USAGE ON SCHEMA public TO jikim;

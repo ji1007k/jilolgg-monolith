ea3461da2eff230574df5b017bd17fb48d3157ed
jungin <wjddls141093@duksung.ac.kr>
2025-04-25 15:02:46 +0900

    1. fix(db): 사용자 생성 쿼리 오류 수정

--- src/main/resources/db/postgres/init.sql
+++ src/main/resources/db/postgres/init.sql
@@ -1,13 +1,21 @@
--- jikim 유저 생성 (이미 존재하면 무시)
-DO $$ BEGIN
-CREATE ROLE jikim WITH LOGIN PASSWORD 'jikim';
-EXCEPTION WHEN OTHERS THEN
-   RAISE NOTICE 'Role jikim already exists';
+-- 사용자 생성 (이미 있으면 건너뛰기)
+DO $$
+BEGIN
+    -- 사용자 존재 여부 확인
+    IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_roles WHERE rolname = 'jikim') THEN
+        -- 사용자 생성
+        CREATE USER jikim WITH PASSWORD 'jikim';
+    END IF;
 END $$;
 
--- jikim 유저에게 데이터베이스 권한 부여
-ALTER DATABASE basic OWNER TO jikim;
+-- 데이터베이스 권한 부여
 GRANT ALL PRIVILEGES ON DATABASE basic TO jikim;
 
--- 새 테이블을 만들거나 데이터를 삽입할 수 있도록 권한 부여
-GRANT ALL ON SCHEMA public TO jikim;
+-- 스키마 내 권한 부여 (기존 권한 부여 여부 확인 불필요)
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO jikim;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO jikim;
+GRANT USAGE ON SCHEMA public TO jikim;
+
+-- 앞으로 생성될 테이블과 시퀀스에 대한 기본 권한 설정
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO jikim;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO jikim;

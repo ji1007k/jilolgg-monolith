547c7418973e9db7f3f828ae43edf6adc9024129
jungin <wjddls141093@duksung.ac.kr>
2025-05-02 15:26:21 +0900

    1. [Refactor] 스탠딩 데이터 세분화 - 스테이지, 섹션 DTO 추가
    2. [Feat] LOL 순위 산정 로직 변경
    - 승리 수 우선 정렬 + 득실차 보조 정렬 적용

--- src/main/java/com/test/basic/lol/standings/StandingsDto.java
+++ src/main/java/com/test/basic/lol/standings/StandingsDto.java
@@ -1,17 +1,12 @@
 package com.test.basic.lol.standings;
 
-import com.test.basic.lol.teams.Team;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class StandingsDto {
-    private String id;        // 113503303283548977
-    private String tournamentId;
-    private String name;    // 정규 리그
-    private String slug;    // regular_season
-    private String type;
+    private String id;      // tournamentId
 
-    private List<Team> rankings;
+    private List<StageDto> stages;
 }

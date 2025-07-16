9e350eba464f6abe0bdce41665d1a09e9503a64d
jungin <wjddls141093@duksung.ac.kr>
2025-07-16 16:17:29 +0900

    [fix] syncMatchJob 테스트 시 동기식 jobLauncher 적용 - job 완료 후 응답 수신 및 상태코드 확인

--- src/test/java/com/test/basic/lol/batch/SyncMatchJobTest.java
+++ src/test/java/com/test/basic/lol/batch/SyncMatchJobTest.java
@@ -1,11 +1,10 @@
 package com.test.basic.lol.batch;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +13,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.time.Year;
 import java.time.ZoneId;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,22 +30,43 @@ public class SyncMatchJobTest {
     private JobLauncherTestUtils jobLauncherTestUtils;
 
     @Autowired
+    private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+    @Autowired
     private Job syncMatchJob;
+
+    @Autowired
+//    @Qualifier("asyncJobLauncher")
+    private JobLauncher jobLauncher;    // job 완료 후 응답받기 위해 기본 JobLauncher 주입
 
     @BeforeAll
     void setUp() {
+        jobLauncherTestUtils.setJobLauncher(jobLauncher);
         jobLauncherTestUtils.setJob(syncMatchJob);
+    }
+
+    @AfterEach
+    void tearDown() {
+        // 1. 테스트 후 Job 실행 기록 삭제
+        // BATCH_JOB_EXECUTION 테이블에서 모든 기록 삭제
+        // BATCH_JOB_EXECUTION_CONTEXT 테이블에서 모든 기록 삭제
+        // BATCH_JOB_EXECUTION_PARAMS 테이블에서 모든 기록 삭제
+        // BATCH_JOB_INSTANCE 테이블에서 모든 기록 삭제
+        // BATCH_STEP_EXECUTION 테이블에서 모든 기록 삭제
+        // BATCH_STEP_EXECUTION_CONTEXT 테이블에서 모든 기록 삭제
+        jobRepositoryTestUtils.removeJobExecutions();
     }
 
 
     // @SpringBatchTest + JobLauncherTestUtils
     @Test
-    @DisplayName("전체 Batch Job 테스트(통합)")
+    @DisplayName("전체 Batch Job 테스트(통합) - 동기식")
     void syncMatchJob_runsSuccessfully() throws Exception {
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString("leagueId", "98767991302996019")
                 .addLong("targetYear", (long) Year.now(ZoneId.of("Asia/Seoul")).getValue())
-                .addLong("time", System.currentTimeMillis())    // 유일한 job instance 위해 시간 파라미터 추가
+                .addLong("time", System.currentTimeMillis())        // 유일한 job instance 위해 시간 파라미터 추가
+                .addString("uuid", UUID.randomUUID().toString())    // 시간은 중복될 수 있어서 식별을 위한 UUID 추가
                 .toJobParameters();
 
         JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);

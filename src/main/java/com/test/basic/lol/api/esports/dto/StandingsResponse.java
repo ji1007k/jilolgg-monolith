16d8502909db07fb05d78a6cc2f150a5ea534c84
jungin <wjddls141093@duksung.ac.kr>
2025-07-24 19:55:32 +0900

    [fix] 순위표 캐시 데이터 역직렬화 오류 수정  - 이너클래스에 기본 생성자 어노테이션 추가  - Data 어노테이션 제거

--- src/main/java/com/test/basic/lol/api/esports/dto/StandingsResponse.java
+++ src/main/java/com/test/basic/lol/api/esports/dto/StandingsResponse.java
@@ -1,27 +1,37 @@
 package com.test.basic.lol.api.esports.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class StandingsResponse {
 
     private StandingsData data;
 
-    @Data
+    @Getter
+    @Setter
+    @NoArgsConstructor
     public static class StandingsData {
         private String tournamentId;      // tournamentId
         List<StandingsDto> standings;
     }
 
-    @Data
+    @Getter
+    @Setter
+    @NoArgsConstructor
     public static class StandingsDto {
         public List<StageDto> stages;
     }
 
-    @Data
+    @Getter
+    @Setter
+    @NoArgsConstructor
     public static class StageDto {
         public String id;
         public String name;
@@ -30,7 +40,9 @@ public class StandingsResponse {
         public List<SectionDto> sections;
     }
 
-    @Data
+    @Getter
+    @Setter
+    @NoArgsConstructor
     public static class SectionDto {
         public String name;
         public List<MatchDto> matches;
@@ -38,7 +50,9 @@ public class StandingsResponse {
         public List<TeamDto> refinedRankings;    // 공동순위 처리 후 flatMap한 결과 리스트
     }
 
-    @Data
+    @Getter
+    @Setter
+    @NoArgsConstructor
     public static class MatchDto {
         @JsonProperty("id")
         public String matchId;
@@ -61,7 +75,9 @@ public class StandingsResponse {
         }
     }
 
-    @Data
+    @Getter
+    @Setter
+    @NoArgsConstructor
     public static class TeamDto {
         // WRITE_ONLY: JSON에서 받을 때만 id로 사용 (역직렬화)
         @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
@@ -81,21 +97,27 @@ public class StandingsResponse {
         }
     }
 
-    @Data
+    @Getter
+    @Setter
+    @NoArgsConstructor
     public static class ResultDto {
         public String outcome;
         public int gameWins;
     }
 
 
-    @Data
+    @Getter
+    @Setter
+    @NoArgsConstructor
     public static class RecordDto {
         public int wins;
         public int losses;
         public int gameDiff;
     }
 
-    @Data
+    @Getter
+    @Setter
+    @NoArgsConstructor
     public static class RankingDto {
         public int ordinal;
         public List<TeamDto> teams;
